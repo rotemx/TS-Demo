e5fb808bb9476e766e0f53fe7653170874401034
Rotem Meiri <rotemx@users.noreply.github.com>
2022-06-08 21:11:33 +0300

    fixed sql save

--- data-saver/js/web-sql-manager.js
+++ data-saver/js/web-sql-manager.js
@@ -14,9 +14,9 @@ export class WebSqlManager {
         return __awaiter(this, void 0, void 0, function* () {
             console.log("sql init");
             return new Promise((resolve, reject) => {
-                this.db = window.openDatabase("mydb6", "1.0", "My DB", 2 * 1024 * 1024, () => {
+                this.db = window.openDatabase(Date.now().toString(), "1.0", "My DB", 2 * 1024 * 1024, () => {
                     this.db.transaction(function (tx) {
-                        tx.executeSql("CREATE TABLE IF NOT EXISTS Items(key UNIQUE, value)", [], function (tx) {
+                        tx.executeSql("CREATE TABLE IF NOT EXISTS ITEMS(key UNIQUE, value)", [], function (tx) {
                             console.log('Table created');
                             resolve();
                         }, null);
@@ -29,7 +29,13 @@ export class WebSqlManager {
     save(key, value) {
         console.log("saving to WebSQL...");
         this.db.transaction(function (tx) {
-            tx.executeSql('INSERT INTO Items (key,value) VALUES ("myKey", "myValuer"'), [key, value];
+            console.log('inserting...');
+            tx.executeSql('INSERT INTO ITEMS (key,value) VALUES (?,?)', [key, value], () => {
+                console.log('sql statement cb SUCCESS');
+            }, function (tx) {
+                console.log(tx, ' ERR tx');
+                return false;
+            });
         });
     }
     load(key) {
--- data-saver/src/web-sql-manager.ts
+++ data-saver/src/web-sql-manager.ts
@@ -8,9 +8,9 @@ export class WebSqlManager {
 	async init(): Promise<void> {
 		console.log("sql init");
 		return new Promise((resolve, reject) => {
-			this.db = window.openDatabase("mydb6", "1.0", "My DB", 2 * 1024 * 1024, () => {
+			this.db = window.openDatabase(Date.now().toString(), "1.0", "My DB", 2 * 1024 * 1024, () => {
 				this.db.transaction(function (tx) {
-					tx.executeSql("CREATE TABLE IF NOT EXISTS Items(key UNIQUE, value)", [], function (tx) {
+					tx.executeSql("CREATE TABLE IF NOT EXISTS ITEMS(key UNIQUE, value)", [], function (tx) {
 						console.log('Table created');
 						resolve();
 					}, null);
@@ -24,7 +24,13 @@ export class WebSqlManager {
 		console.log("saving to WebSQL...");
 		
 		this.db.transaction(function (tx) {
-			tx.executeSql('INSERT INTO Items (key,value) VALUES ("myKey", "myValuer"'), [key, value];
+			console.log('inserting...');
+			tx.executeSql('INSERT INTO ITEMS (key,value) VALUES (?,?)',[key,value],()=>{
+			    console.log('sql statement cb SUCCESS');
+			}, function (tx){
+			    console.log(tx, ' ERR tx');
+				return false
+			}) ;
 		});
 		
 	}
