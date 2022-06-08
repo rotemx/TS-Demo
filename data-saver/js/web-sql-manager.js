e7402f0a6639299b0b5f21e5f14a1d617bdc5f62
Rotem Meiri <rotemx@users.noreply.github.com>
2022-06-08 20:03:55 +0300

    added data-saver FE project

--- data-saver/js/web-sql-manager.js
+++ data-saver/js/web-sql-manager.js
@@ -1,8 +1,10 @@
 export class WebSqlManager {
     save(key, value) {
         console.log('saving to WebSQL...');
+        //todo: implement fn
     }
     load(key) {
+        //todo: implement fn
         return 'data from WebSQL';
     }
 }
--- data-saver/src/web-sql-manager.ts
+++ data-saver/src/web-sql-manager.ts
@@ -1,12 +1,15 @@
 export class WebSqlManager {
 	save(key:string, value:any):void{
 		console.log('saving to WebSQL...');
+		//todo: implement fn
 	}
 	
 	
 	
 	
 	load(key:string):any{
+		//todo: implement fn
+		
 		return 'data from WebSQL'
 	}
 }
